4b40a15002cb295923164954393bbe10b9173c02
Chatri Sae-Tung <gearmag@gmail.com>
2013-12-22 14:28:48 +0700

    remove unused comment.

--- todo/web/static/web/angular/js/controller.js
+++ todo/web/static/web/angular/js/controller.js
@@ -228,24 +228,3 @@ App.directive('datepicker', function() {
 		}
 	};
 });
-
-/*
- App.directive('datepicker', function() {
- return {
- restrict : 'A',
- require : 'ngModel',
- link : function(scope, element, attrs, ngModelCtrl) {
- $(function() {
- element.datepicker({
- dateFormat : 'mm.dd.yy',
- showOn: "both",
- buttonText: "<i class='fa fa-calendar'></i>",
- onSelect : function(date) {
- ngModelCtrl.$setViewValue(date);
- scope.$apply();
- }
- });
- });
- }
- };
- });*/
